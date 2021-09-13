ff4cb94d07c490398bb03c9f42e3703de21f9084
Andrew Wang <ay.wang@mail.utoronto.ca>
2021-09-12 20:46:55 -0400

    match casing

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const EXCLUDED_TAGS = ['STYLE', 'CODE', 'SCRIPT', 'NOSCRIPT'];
+const excludedTags = ['STYLE', 'CODE', 'SCRIPT', 'NOSCRIPT'];
 
 const dict = {
     'color': 'colour',
     'center': 'centre'
 };
 
-replaceText(document.body)
+replaceText(document.body);
 
 function replaceText(element) {
-    if (EXCLUDED_TAGS.includes(element.tagName)) return;
+    if (excludedTags.includes(element.tagName)) return;
+
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
@@ -23,10 +24,24 @@ function isMono(element) {
 }
 
 function replaceWord(element, word) {
-    const re = new RegExp(word, 'gi')
+    const re = new RegExp(word, 'gi');
     if (element.textContent.match(re) && !isMono(element)) {
         const newElement = document.createElement('span');
-        newElement.innerHTML = element.textContent.replace(re, dict[word]);
+        newElement.innerHTML = element.textContent.replace(re, matchCaseReplacer(word));
         element.replaceWith(newElement);
     }
+}
+
+function matchCaseReplacer(word) {
+    const newWord = dict[word];
+    return match => {
+        if (match === word.toUpperCase()) {
+            // uppercase
+            return newWord.toUpperCase();
+        } else if (match === word.charAt(0).toUpperCase() + word.slice(1)) {
+            // capitalized
+            return newWord.charAt(0).toUpperCase() + newWord.slice(1);
+        }
+        return newWord;
+    };
 }
